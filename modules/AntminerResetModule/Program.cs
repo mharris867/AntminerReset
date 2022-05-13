04cd8f3226ef966c156f56ddc216a54c0d3ae4af
Mark McPhail <mmcphail@starbucks.com>
2022-05-13 15:09:57 -0700

    running

--- modules/AntminerResetModule/Program.cs
+++ modules/AntminerResetModule/Program.cs
@@ -55,7 +55,7 @@ namespace AntminerResetModule
             // Run Powershell
             //PowerShell ps = PowerShell.Create();
             //ps.AddArgument()
-            PowerShell.Create().AddScript(@"SoftReset.ps1").AddArgument("-noninteractive").Invoke();
+            //PowerShell.Create().AddScript(@"SoftReset.ps1").AddArgument("-noninteractive").Invoke();
             
         }
 
